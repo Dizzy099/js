ce79952134d666af301b60d84c8af63e003428d0
Awanish Mishra <105626524+Dizzy099@users.noreply.github.com>
2024-08-31 08:25:12 +0000

    datatypes summarized

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,40 @@
+// primitive
+
+// 7 : string, number, boolean, null, undefined, Symbol, bigInt
+
+const score = 100
+
+const myEmail = "hello@mail.com" 
+
+const isLoggedIn = false
+
+const outsideTemp = null  // object- type
+
+let isEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId)
+
+const bigNumber = 58627582645688886n
+
+
+// Reference type 
+
+// 3 : Array, Objects, Function
+
+const myArray = ["shazam","iron-man", "spidey"];
+
+let myObject = {
+    name : "Awanish",
+    age : 18,
+
+}
+
+const myFunction = function(){
+    console.log("Hello World!!");
+    
+}
+
+console.log(typeof isEmail );
