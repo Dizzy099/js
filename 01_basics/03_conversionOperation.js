80289be0d6b87f40c38b163a6c18cf643444374a
Awanish Mishra <105626524+Dizzy099@users.noreply.github.com>
2024-08-31 07:23:45 +0000

    conversion

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -35,14 +35,51 @@ let booleanLoggedIn = Boolean(isLoggedIn)
 
 let someNumber = 33
 
-console.log(typeof someNumber);
+// console.log(typeof someNumber);
 
 
 let stringNumber  = String(someNumber)
 
-console.log(stringNumber);
+// console.log(stringNumber);
 
-console.log(typeof stringNumber);
+// console.log(typeof stringNumber);
+
+
+/*         Operations              */
+
+let value = 66
+
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2/2);
+// console.log(2**3);
+// console.log(89 % 15);
+
+let str1 = "hello"
+let str2 = "_world"
+let str3 = str1 + str2
+
+
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 3);
+// console.log(1 + 2 + "3");
+
+// console.log(+true);
+
+
+
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+
 
 
 
