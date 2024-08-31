612647e2cae5a79049952926ce10232162a7c4e6
Awanish Mishra <105626524+Dizzy099@users.noreply.github.com>
2024-08-31 03:29:12 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,24 @@
+const accountId = 41556
+
+let accountEmail = "awanish@gmail.com"
+
+var accountPassword = "sdfyui"
+
+let accountState;
+
+accountCity  = "jaipur"
+
+accountEmail = "gvs@mail.com"
+accountPassword = "5383"
+accountCity = "hajipur"
+
+// accountId = 2 not allowed for const var
+
+/* 
+  We dont use var scope in javascript because of block scope & fucntional scope
+
+*/
+
+console.log(accountId);
+console.table([accountEmail, accountPassword, accountCity, accountId, accountState]);
+
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all code as newer version
+
+// alert(1 + 5) // we are using nodejs & not browser
+
+let name = "Awanish"
+
+let age = 18
+
+let isLoggedIn = false
+
+let state;
+
+// number 2 to power 53
+// bigint
+// string => ""
+// boolean => true or false
+// null => standalone value
+// undefined
+// symbol 
+
+// object
+
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
