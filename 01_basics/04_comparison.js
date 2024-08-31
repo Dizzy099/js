3473ab5f568201152de5c4f03475a46d20145124
Awanish Mishra <105626524+Dizzy099@users.noreply.github.com>
2024-08-31 07:56:53 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,31 @@
+// console.log(1>2);
+// console.log(3>2);
+// console.log(1==2);
+// console.log(3!=2);
+
+// console.log("1">2);
+// console.log("03">2);
+
+// console.log(null>0);
+// console.log(null<0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined > 0);
+// console.log(undefined < 0);
+// console.log(undefined == 0);
+// console.log(undefined >= 0);
+
+// avoid these type of conversions : ^^
+
+// ===
+
+console.log("2" === 2);
+
+
+
+
+
+
+
+
