b9bff2b56b1d0ff1247ea12eff38146802d61353
Awanish Mishra <105626524+Dizzy099@users.noreply.github.com>
2024-08-31 05:28:10 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,48 @@
+let score  = false
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+
+// console.log(valueInNumber); // Not a Number NaN
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1 & false => 0
+
+
+let isLoggedIn = "awanish"
+
+let booleanLoggedIn = Boolean(isLoggedIn)
+
+// console.log(isLoggedIn);
+
+// console.log(typeof isLoggedIn);
+
+// console.log(booleanLoggedIn);
+
+//   console.log(typeof booleanLoggedIn);
+
+// 1 => true ; 0 => false
+
+// "" => false
+// "awanish" => true
+
+let someNumber = 33
+
+console.log(typeof someNumber);
+
+
+let stringNumber  = String(someNumber)
+
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
+
+
+
